c22ffa924ab4fa82142e442ec8b1e1a69b1a6a80
pabra <pabra@netzfruech.de>
2017-11-07 22:33:04 +0100

    playing with list transitions to animate stations add/remove

--- src/components/Stations.vue
+++ src/components/Stations.vue
@@ -8,15 +8,12 @@
             :debounce=1000
         )
 
-        table.u-full-width
-            //- thead
-            //-     tr
-            //-         th Station
-            //-         th Action
-            tbody
-                tr(v-if="foundStations.length")
+        table.u-full-width(v-if="foundStations.length")
+            thead
+                tr
                     th(colspan="2") found stations
 
+            transition-group(name="list" tag="tbody")
                 tr(v-for="station in foundStations" :key="station.id")
                     td
                         a(href="#" @click.prevent="showDeparture(station)")
@@ -25,10 +22,17 @@
                         button.favorites.add(title="add to favorites" @click="addStation(station)")
                             i.material-icons
 
-                tr(v-if="sortedFavoriteStations.length")
+        table.u-full-width(v-if="sortedFavoriteStations.length")
+            thead
+                tr
                     th(colspan="2") favorite stations
 
-                tr(v-for="station in sortedFavoriteStations" :key="station.id")
+            transition-group(name="list" tag="tbody")
+                tr(
+                    v-for="station of sortedFavoriteStations"
+                    :key="station.id"
+                    :class="{highlight: hightlightStations.indexOf(station.id) !== -1}"
+                )
                     td
                         a(href="#" @click.prevent="showDeparture(station)")
                             | {{ station.city }}, {{ station.stop }}
@@ -49,15 +53,21 @@
             return {
                 foundStations: [],
                 findStation: '',
+                hightlightStations: [],
             };
         },
         computed: {
             ...mapGetters(['sortedFavoriteStations']),
         },
         methods: {
+            highlight(station) {
+                this.hightlightStations.push(station.id);
+                setTimeout(() => { this.hightlightStations.shift(); }, 200);
+            },
             addStation(station) {
                 window.console.log('station', station);
                 this.$store.commit('addStation', station);
+                this.highlight(station);
             },
             removeStation(station) {
                 window.console.log('station', station);
@@ -93,10 +103,32 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/assets/scss/variables.scss";
+
     table {
+        tr.highlight {
+            td {
+                background-color: $main-color-very-light;
+                transition-duration: 200ms;
+            }
+        }
+
+        tr.list-move {
+            transition: all 500ms;
+        }
+
+        tr.list-enter, .list-leave-to {
+            opacity: 0;
+            transform: translateX(-30px);
+        }
+        tr.list-leave, tr.list-leave-active {
+            position: absolute;
+        }
+
         th,
         td {
             padding: 2px 0;
+            transition-duration: 2s;
 
             &:first-child {
                 width: 100%;
