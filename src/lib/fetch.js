ee60b07f3c62c4e9678e530f4fa07502fca19f1c
pabra <pabra@netzfruech.de>
2017-12-11 21:10:08 +0100

    add common fetch helpers isApiResponseOk and parseApiDate

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,6 +1,7 @@
 // https://gist.github.com/kiliankoe/928c3ddf851fb47aa62bdc950ca5bb56
 // https://github.com/kiliankoe/vvo/wiki/WebAPI
 
+import _ from 'lodash';
 import { isObject, GK4toWGS84, parseMot } from '@/lib/utils';
 import store from '@/store';
 
@@ -13,17 +14,21 @@ function handleError(err) {
     };
 }
 
-// const response = require('./response.json');
-//
-// async function fetchJson() {
-//     return {
-//         ok: true,
-//         error: handleError(isObject('test')),
-//         text: '',
-//         data: response,
-//         status: 200,
-//     };
-// }
+function isApiResponseOk(res) {
+    return [
+        _.isPlainObject(res),
+        res.ok,
+        _.get(res, 'data.Status.Code', '').toLowerCase() === 'ok',
+    ].every(entry => !!entry);
+}
+
+function parseApiDate(string) {
+    const match = string.match(/Date\((\d{13})\+\d{4}\)/);
+    if (!match) throw TypeError(`unable to parse Date in "${string}"`);
+
+    return new Date(parseInt(match[1], 10));
+}
+
 async function fetchJson(options) {
     const defaults = {
         url: null,
@@ -88,24 +93,24 @@ export async function fetchSations(query) {
         },
     });
 
-    if (!res.ok || !res.data || !res.data.Status || !res.data.Points || !res.data.Status.Code || res.data.Status.Code.toLowerCase() !== 'ok') {
+    if (!isApiResponseOk(res) || !res.data.Points) {
         throw new Error(`not ok result ${JSON.stringify(res)}`);
-    } else {
-        return res.data.Points
-            .map(p => p.split('|'))
-            .filter(p => p[3])
-            // remove duplicates
-            .filter((point, idx, arr) => idx === arr.findIndex(p => p[0] === point[0]))
-            .map((p) => {
-                const city = p[2] === '' ? 'Dresden' : p[2];
-                return {
-                    city,
-                    stop: p[3],
-                    id: p[0],
-                    coords: GK4toWGS84(parseInt(p[4], 10), parseInt(p[5], 10)),
-                };
-            });
     }
+
+    return res.data.Points
+        .map(p => p.split('|'))
+        .filter(p => p[3])
+        // remove duplicates
+        .filter((point, idx, arr) => idx === arr.findIndex(p => p[0] === point[0]))
+        .map((p) => {
+            const city = p[2] === '' ? 'Dresden' : p[2];
+            return {
+                city,
+                stop: p[3],
+                id: p[0],
+                coords: GK4toWGS84(parseInt(p[4], 10), parseInt(p[5], 10)),
+            };
+        });
 }
 
 export async function fetchDeparture(stationId, offset = 0, limit = 30) {
@@ -126,42 +131,39 @@ export async function fetchDeparture(stationId, offset = 0, limit = 30) {
         },
     });
     window.console.log('res', res);
-    // debugger;
 
-    if (!res.ok || !res.data || !res.data.Status) {
-        throw new Error(`not ok result ${JSON.stringify(res)}`);
-    } else {
-        if (!res.data.Departures) res.data.Departures = [];
+    if (!isApiResponseOk(res)) throw new Error(`not ok result ${JSON.stringify(res)}`);
 
-        return {
-            city: res.data.Place,
-            stop: res.data.Name,
-            status: res.data.Status,
-            departures: res.data.Departures.map((d) => {
-                // window.console.log('d', d);
-                const arrivalTime = new Date(parseInt(d.RealTime ? d.RealTime.match(/\d+/)[0] : d.ScheduledTime.match(/\d+/)[0], 10));
-                const scheduledTime = new Date(parseInt(d.ScheduledTime.match(/\d+/)[0], 10));
+    if (!res.data.Departures) res.data.Departures = [];
 
-                return {
-                    arrivalTime,
-                    scheduledTime,
-                    line: d.LineName,
-                    direction: d.Direction,
-                    platform: d.Platform ?
-                        { name: d.Platform.Name, type: d.Platform.Type } :
-                        {},
-                    arrivalTimeRelative: Math.round((arrivalTime - now) / 1000 / 60),
-                    scheduledTimeRelative: Math.round((scheduledTime - now) / 1000 / 60),
-                    delayTime: Math.round((arrivalTime - scheduledTime) / 1000 / 60),
-                    state: d.State ? d.State : 'Unknown',
-                    mode: parseMot(d.Mot),
-                    diva: d.Diva ?
-                        { number: parseInt(d.Diva.Number, 10), network: d.Diva.Network } :
-                        {},
-                };
-            }),
-        };
-    }
+    return {
+        city: res.data.Place,
+        stop: res.data.Name,
+        status: res.data.Status,
+        departures: res.data.Departures.map((d) => {
+            // window.console.log('d', d);
+            const scheduledTime = parseApiDate(d.ScheduledTime);
+            const arrivalTime = d.RealTime ? parseApiDate(d.RealTime) : scheduledTime;
+
+            return {
+                arrivalTime,
+                scheduledTime,
+                line: d.LineName,
+                direction: d.Direction,
+                platform: d.Platform ?
+                    { name: d.Platform.Name, type: d.Platform.Type } :
+                    {},
+                arrivalTimeRelative: Math.round((arrivalTime - now) / 1000 / 60),
+                scheduledTimeRelative: Math.round((scheduledTime - now) / 1000 / 60),
+                delayTime: Math.round((arrivalTime - scheduledTime) / 1000 / 60),
+                state: d.State ? d.State : 'Unknown',
+                mode: parseMot(d.Mot),
+                diva: d.Diva ?
+                    { number: parseInt(d.Diva.Number, 10), network: d.Diva.Network } :
+                    {},
+            };
+        }),
+    };
 }
 
 // for debugging in browser
