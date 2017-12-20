086e0691812a11e3a1f4192e9e3af91e0cc4e071
pabra <pabra@netzfruech.de>
2017-12-20 20:53:32 +0100

    compress more files but only if size > 512 byte

--- compress-dist.sh
+++ compress-dist.sh
@@ -8,13 +8,17 @@ GZIP=$(which pigz 2>/dev/null)
 [ ! -x "$GZIP" ] && echo 'no `pigz`/`gzip` found' && exit 1
 TOUCH=$(which touch 2>/dev/null)
 [ ! -x "$TOUCH" ] && echo 'no `touch` found' && exit 1
+WC=$(which wc 2>/dev/null)
+[ ! -x "$WC" ] && echo 'no `wc` found' && exit 1
 
 find $DIST_DIR -type f -print0 | while IFS= read -r -d $'\0' file; do
     ext="${file##*.}"
     case $ext in
-        html|js|css|map|svg|ttf|eot)
-            $GZIP -9 -c $file > ${file}.gz
-            $TOUCH -r $file ${file}.gz
+        html|js|css|map|svg|ttf|eot|json|ico|webapp)
+            if [ "$($WC -c < $file)" -gt 512 ]; then
+                $GZIP -9 -c $file > ${file}.gz
+                $TOUCH -r $file ${file}.gz
+            fi
             ;;
     esac
 done
