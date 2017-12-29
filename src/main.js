3c1e92034882bfa9ca0998c1694f966a5f4f8771
pabra <pabra@netzfruech.de>
2017-12-29 17:28:09 +0100

    add Date.new to polyfills

--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ function browserSupportsAllFeatures() {
         typeof Object.assign === 'function' &&
         typeof Array.prototype.find === 'function' &&
         typeof Array.prototype.findIndex === 'function' &&
+        typeof Date.now === 'function' &&
         typeof window.Set === 'function'
     );
 }
--- src/polyfills.js
+++ src/polyfills.js
@@ -3,4 +3,5 @@ require('core-js/modules/es6.promise');
 require('core-js/modules/es6.object.assign');
 require('core-js/modules/es6.array.find');
 require('core-js/modules/es6.array.find-index');
+require('core-js/modules/es6.date.now');
 require('core-js/modules/es6.set');
