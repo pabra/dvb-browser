cc1b8b04794b310ce1ccca6e1ab5dbe49dca4ef2
pabra <pabra@netzfruech.de>
2018-01-14 11:08:18 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "$0")" && pwd -P)"
+SYNC_SRC=${DIR}/dist/
+SYNC_DST=/var/www/dvb/
+SYNC_HOST=dvb.peppnet.de
+
+rsync \
+    -avP \
+    --delete \
+    ${SYNC_SRC} \
+    $SYNC_HOST:${SYNC_DST}
