2baca32bc2248cba8efe646c8cdb66d7702fae38
pabra <pabra@netzfruech.de>
2019-05-12 13:10:50 +0200

    compress with brotli and zopfli

--- compress-dist.sh
+++ compress-dist.sh
@@ -3,6 +3,9 @@
 DIR="$(cd "$(dirname "$0")" && pwd -P)"
 DIST_DIR=${DIR}/dist
 
+BROTLI=$(command -v brotli)
+ZOPFLI=$(command -v zopfli)
+
 GZIP=$(command -v pigz)
 [ ! -x "$GZIP" ] && GZIP=$(command -v gzip)
 [ ! -x "$GZIP" ] && echo "no 'pigz'/'gzip' found" && exit 1
@@ -16,8 +19,33 @@ find "$DIST_DIR" -type f -print0 | while IFS= read -r -d $'\0' file; do
     case $ext in
         html|js|css|map|svg|ttf|eot|json|ico|webapp)
             if [ "$($WC -c < "$file")" -gt 512 ]; then
-                $GZIP -9 -c "$file" > "${file}.gz"
-                $TOUCH -r "$file" "${file}.gz"
+                if [ -x "$BROTLI" ]; then
+                    outfile="${file}.br"
+                    (
+                        (
+                            increment "$outfile start" &&
+                            $BROTLI -c "$file" > "$outfile" &&
+                            $TOUCH -r "$file" "$outfile"
+                        ) &&
+                        decrement "$outfile done"
+                    ) &
+                fi
+
+                if [ -x "$ZOPFLI" ]; then
+                    outfile="${file}.gz"
+                    (
+                        (
+                            increment "$outfile start" &&
+                            $ZOPFLI -c "$file" > "$outfile" &&
+                            $TOUCH -r "$file" "$outfile"
+                        ) &&
+                        decrement "$outfile done"
+                    ) &
+                else
+                    echo "${file}.gz"
+                    $GZIP -9 -c "$file" > "${file}.gz"
+                    $TOUCH -r "$file" "${file}.gz"
+                fi
             fi
             ;;
     esac
